992a261151610615635353c15374fcb36bf4ea3b
AbrahamJoseGringo <aperez.castro32@gmail.com>
2024-08-27 14:16:39 -0300

    Api

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,3 +1,5 @@
-from .user import UserSerializer
+from .autor import AutorSerializer
 from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
+from .livro import LivroDetailSerializer, LivroListSerializer, LivroSerializer
+from .user import UserSerializer
