a393851107ec0971e87897d13ed0f9993da359d6
AbrahamJoseGringo <aperez.castro32@gmail.com>
2024-10-01 16:11:10 -0300

    Manhwas api

--- core/models/manhwa.py
+++ core/models/manhwa.py
@@ -1,23 +1,58 @@
 from django.db import models
-
-from uploader.models import Image
-
+from uploader.models import Image, Document
 from .categoria import Categoria
 from .tipo import Tipo
 
 
+LANGUAGE_CHOICES = [
+    ('PT', 'Português'),
+    ('EN', 'Inglês'),
+    ('ES', 'Espanhol'),
+    ('JP', 'Japonês'),
+]
+
 class Manhwa(models.Model):
-    titulo = models.CharField(max_length=255)
-    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="manhwas", blank=True, null=True)
-    tipo = models.ForeignKey(Tipo, on_delete=models.PROTECT, related_name="manhwas", blank=True, null=True)
+    numero = models.IntegerField(unique=True)  # Adicione unique=True se cada Manhwa tiver um número único
+    titulo = models.CharField(max_length=255, blank=True, null=True)
+    idioma = models.CharField(max_length=2, choices=LANGUAGE_CHOICES, default='PT')  # Define choices para idioma
+
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.PROTECT,
+        related_name="manhwas",
+        blank=True,
+        null=True
+    )
+
+    tipo = models.ForeignKey(
+        Tipo,
+        on_delete=models.PROTECT,
+        related_name="manhwas",
+        blank=True,
+        null=True
+    )
+
     capa = models.ForeignKey(
         Image,
-        related_name="+",
         on_delete=models.CASCADE,
         null=True,
         blank=True,
         default=None,
     )
 
+    documento = models.ForeignKey(
+        Document,
+        on_delete=models.CASCADE,
+        related_name="capitulos",
+        null=True,
+        blank=True,
+    )
+
+    data_publicacao = models.DateField(auto_now_add=True)
+
     def __str__(self):
-        return f"({self.id}) {self.titulo}"
+        return f"({self.id}) {self.titulo or 'Sem título'} - Capítulo {self.numero}"
+
+    class Meta:
+        ordering = ['numero']  
+        unique_together = ('numero', 'titulo') 
